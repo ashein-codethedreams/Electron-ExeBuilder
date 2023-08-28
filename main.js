f42a69b1e10672f08eaafb80be8f1af24148ebb4
unknown <heroglasses155@gmail.com>
2023-08-29 00:35:20 +0630

    electron exe win64 with electron-builder

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true, // Necessary for Next.js
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000"); // Assuming your Next.js app runs on this port
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
